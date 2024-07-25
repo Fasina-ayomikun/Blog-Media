1e9863df6875bca6e6ed212694177b57fd6e1013
fasina-ayomikun <ayomikunfasina@yahoo.com>
2024-07-25 18:00:46 +0100

    initial commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage, ref } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyAaZ9adL6ixVDIrzuo4C87QgFjDuw84-wQ",
-  authDomain: "blog-media-c27bc.firebaseapp.com",
-  projectId: "blog-media-c27bc",
-  storageBucket: "blog-media-c27bc.appspot.com",
-  messagingSenderId: "340644064082",
-  appId: "1:340644064082:web:72a9685805809b9a43a88b",
-  measurementId: "G-N3TCS00RB4",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
